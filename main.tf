5bdcfb36a2246efe1064cf859c7e9d135b7c06ab
Gary Hawes <gary.hawes@gmail.com>
2020-10-06 17:54:15 +0100

    Changing main.tf to use variables and provider

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 resource "aws_vpc" "freecode-vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -122,7 +106,7 @@ resource "aws_instance" "web-server-instance" {
   ami               = var.ec2_ami
   instance_type     = "t2.micro"
   availability_zone = var.availability_zone
-  key_name          = var.ec2_key_name
+  key_name          = "freecode-access-key"
 
   network_interface {
     device_index         = 0
