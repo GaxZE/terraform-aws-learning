f2c92a4d7748a0c7e9cbbc339c9a086de79e99d6
Gary Hawes <gary.hawes@gmail.com>
2020-10-06 18:00:48 +0100

    Adding var descriptions and types

--- variables.tf
+++ variables.tf
@@ -1,12 +1,24 @@
 variable "region" {
+  description = "aws region"
+  type        = string
 }
 variable "access_key" {
+  description = "access key"
+  type        = string
 }
 variable "secret_key" {
+  description = "secret key"
+  type        = string
 }
 variable "availability_zone" {
+  description = "aws availability zone"
+  type        = string
 }
 variable "ec2_ami" {
+  description = "EC2 ami image"
+  type        = string
 }
 variable "ec2_key_name" {
+  description = "ssh key filename"
+  type        = string
 }
